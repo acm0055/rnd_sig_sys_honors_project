7d069a4da577a7f6338aebd61e19263f86912660
acm0055 <acm0055@auburn.edu>
2016-11-07 15:04:50 -0600

    remove fft code

--- echo_rem/echo_rem.m
+++ echo_rem/echo_rem.m
@@ -3,7 +3,6 @@ function crted_audio = echo_rem(wav_path_in, delta_x_in)
 
   [audio, sample_rate] = wavread(wav_path_in);
   audio = audio(:, 1);
-  audio = fft(audio);
   delta_t = delta_x_in / SPEED_OF_SOUND;
 
   %if sample period is greater than delta_x period ignore_function_time_stamp
@@ -20,10 +19,4 @@ function crted_audio = echo_rem(wav_path_in, delta_x_in)
     %disp(var(audio(i:delta_t_in_samples)));
     crted_audio(i) = audio(i) - 2*audio(i)*auto_core(audio, i, delta_t_in_samples + sample_length, delta_t_in_samples) / var(audio(i:i+delta_t_in_samples));
   end
-  
-  disp('Press enter to hear audio');
-  pause;
-  crted_audio = ifft(crted_audio);
-  c_play = audioplayer(crted_audio, sample_rate);
-  play(c_play);
 end
