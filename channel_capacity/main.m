4be6ae3eaca11bb1d5b1428e99d5f483f633edf0
acm0055 <acm0055@auburn.edu>
2016-11-07 20:17:09 -0600

    finished graph

--- channel_capacity/main.m
+++ channel_capacity/main.m
@@ -1,11 +1,15 @@
+PRECISION = 100;
+RETRY_COUNT = 100;
+
 %first avg transmission
 %second error rate of transmission
-A = zeros(2, 100);
+%third avg channel capacity
+A = zeros(3, 100);
 
 temp = 0;
-for i = 1:100
-  for j = 1:100
-    temp = transmit(generate_random_msg(10,4), i/100, 20);
+for i = 1:PRECISION
+  for j = 1:RETRY_COUNT
+    temp = transmit(generate_random_msg(10,4), i/PRECISION, 20);
     if temp == -1
       A(2,i) = A(2,i) + 1;
     else
@@ -13,18 +17,26 @@ for i = 1:100
     end
   end
   %compute avg ignoring failed transmissions
-  temp = 100 - A(2,i);
+  temp = RETRY_COUNT - A(2,i);
   if temp == 0
   %do nothing because A(1,i) will be zero also
   else
-    A(1,i) = A(1,i)/(100-A(2,i));
+    A(1,i) = A(1,i)/(RETRY_COUNT-A(2,i));
   end
 end
 
+%compute channel cap graphics_toolkit
+%min bytes computed by testing for zero error
+min_bytes = transmit(generate_random_msg(10,4), 0, 20);
+A(3,:) = (ones(1,PRECISION)*min_bytes)./A(1,:);
+
 figure
 %bits of info needed for transmission
-subplot(1,2,1);
+subplot(3,1,1);
 plot(A(1,:))
 %number of errors in transmission
-subplot(1,2,2);
+subplot(3,1,2);
 plot(A(2,:))
+%channel capacity
+subplot(3,1,3);
+plot(A(3,:))
