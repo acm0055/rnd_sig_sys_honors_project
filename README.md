b97d1424ae5ae5b382f64c0efc9a3300a5626e9a
acm0055 <acm0055@auburn.edu>
2016-12-01 23:08:44 -0600

    github things

---
+++ README.md
@@ -0,0 +1,3 @@
+Work and papers for my honor's project in Random Signals and Systems. All
+code is under the BSD 3-clause license. All papers are under Creative
+Commons Attribute license.
--- echo_rem/auto_core.m
+++
@@ -1,8 +0,0 @@
-function val = auto_core(time_func_in, pos_in, sample_length_in, delta_t_in)
-  val = 0;
-  for i = (pos_in:pos_in + sample_length_in - delta_t_in)
-    val = val + time_func_in(i)*time_func_in(i+delta_t_in);
-    %disp(time_func_in(i)*time_func_in(i+delta_t_in))
-  end
-  val = val / (sample_length_in - delta_t_in);
-end
